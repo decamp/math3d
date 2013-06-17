91a012692c050190bfd039c21b45db4f0bb32869
decamp <philipdecamp@gmail.com>
2013-06-17 18:32:32 -0400

    Fixed bug in BasicViewportFunc.

--- src/cogmac/math3d/camera/BasicViewportFunc.java
+++ src/cogmac/math3d/camera/BasicViewportFunc.java
@@ -5,7 +5,6 @@ import cogmac.math3d.Matrices;
 public class BasicViewportFunc implements ViewportFunc {
     @Override
     public void computeViewportMat( int[] viewport, int[] tileViewport, double[] outMat ) {
-        int[] f = tileViewport == null ? viewport : tileViewport;
-        Matrices.computeViewportDepthMatrix( f[0], f[1], f[2] - f[0], f[3] - f[1], 0.0, 1.0, outMat );  
+        Matrices.computeViewportDepthMatrix( viewport[0], viewport[1], viewport[2] - viewport[0], viewport[3] - viewport[1], 0.0, 1.0, outMat );  
     }
 }
