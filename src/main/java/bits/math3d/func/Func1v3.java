507b7f210bd0d4cea5b3014419a31e79274897cf
decamp <decamp@users.noreply.github.com>
2014-11-17 14:40:50 -0500

    Added some function interfaces that use vector outputs.

---
+++ src/main/java/bits/math3d/func/Func1v3.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2014. Massachusetts Institute of Technology
+ * Released under the BSD 2-Clause License
+ * http://opensource.org/licenses/BSD-2-Clause
+ */
+
+package bits.math3d.func;
+
+import bits.math3d.Vec3;
+
+
+/**
+ * @author Philip DeCamp
+ */
+public interface Func1v3 {
+    public void apply( float t, Vec3 out );
+}
